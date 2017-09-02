2b822e38e1b8ba575a4d2e55801ed3f1b2cd0908
Andy Grove <andygrove73@gmail.com>
2017-09-02 07:47:31 -0700

    retrieving error message from libsweep correctly now

--- src/lib.rs
+++ src/lib.rs
@@ -12,13 +12,24 @@ struct sweep_error {}
 
 #[link(name = "sweep")]
 extern {
+  fn sweep_error_message(error: *const sweep_error) -> *const c_char;
   fn sweep_is_abi_compatible() -> bool;
   fn sweep_get_version() -> int32_t;
-  fn sweep_device_construct_simple(port: *const c_char, error: *const sweep_error) -> *const sweep_device;
-  fn sweep_error_message(error: *const sweep_error) -> *const c_char;
-  fn sweep_device_start_scanning(device: *const sweep_device, error: *mut sweep_error);
+  fn sweep_device_construct_simple(port: *const c_char, error: *const *mut sweep_error) -> *const sweep_device;
+  fn sweep_device_start_scanning(device: *const sweep_device, error: *const *mut sweep_error);
 }
 
+
+fn check(error: *mut sweep_error) {
+  if (error.is_null()) {
+    println!("No error");
+  } else {
+    unsafe {
+      let error_string = sweep_error_message(error);
+      println!("Sweep error message: {:?}", CStr::from_ptr(error_string));
+    }
+  }
+}
 
 #[cfg(test)]
 mod tests {
@@ -30,17 +41,22 @@ mod tests {
 
         unsafe {
 
-        let err : *mut sweep_error = std::ptr::null_mut();
+        let mut err : *mut sweep_error = std::ptr::null_mut();
 
         let c = sweep_is_abi_compatible();
         println!("sweep_is_abi_compatible returned {:?}", c);
+
         let v = sweep_get_version();
         println!("sweep_get_version returned {:?}", v);
-        let device = sweep_device_construct_simple(CString::new("TBD").unwrap().as_ptr(), err);
-        sweep_device_start_scanning(device, err);
 
-        let error_string = sweep_error_message(err);
-        println!("Sweep error message: {:?}", CStr::from_ptr(error_string));
+        println!("constructing device");
+        let device = sweep_device_construct_simple(CString::new("TBD").unwrap().as_ptr(), &err);
+        check(err);
+
+        println!("start scanning");
+        sweep_device_start_scanning(device, &err);
+        check(err);
+
 
         }
     }
