a2e05aa2c8d669e24a40ad14d891754960ae0438
Andy Grove <andygrove73@gmail.com>
2017-09-02 08:16:11 -0700

    example code works

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,13 @@ use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
 #[repr(C)]
-struct sweep_device {}
+struct sweep_device;
 
 #[repr(C)]
-struct sweep_error {}
+struct sweep_error;
+
+#[repr(C)]
+struct sweep_scan;
 
 #[link(name = "sweep")]
 extern {
@@ -17,6 +20,14 @@ extern {
   fn sweep_get_version() -> int32_t;
   fn sweep_device_construct_simple(port: *const c_char, error: *const *mut sweep_error) -> *const sweep_device;
   fn sweep_device_start_scanning(device: *const sweep_device, error: *const *mut sweep_error);
+  fn sweep_device_stop_scanning(device: *const sweep_device, error: *const *mut sweep_error);
+  fn sweep_device_get_motor_speed(device: *const sweep_device, error: *const *mut sweep_error) -> int32_t;
+  fn sweep_device_get_sample_rate(device: *const sweep_device, error: *const *mut sweep_error) -> int32_t;
+  fn sweep_device_get_scan(device: *const sweep_device, error: *const *mut sweep_error) -> *const sweep_scan;
+  fn sweep_scan_get_number_of_samples(scan: *const sweep_scan) -> int32_t;
+  fn sweep_scan_get_angle(scan: *const sweep_scan, sample: int32_t) -> int32_t;
+  fn sweep_scan_get_distance(scan: *const sweep_scan, sample: int32_t) -> int32_t;
+  fn sweep_scan_get_signal_strength(scan: *const sweep_scan, sample: int32_t) -> int32_t;
 }
 
 
@@ -53,10 +64,31 @@ mod tests {
         let device = sweep_device_construct_simple(CString::new("/dev/ttyUSB0").unwrap().as_ptr(), &err);
         check(err).unwrap();
 
+        println!("Motor speed: {}", sweep_device_get_motor_speed(device, &err));
+        check(err).unwrap();
+
+        println!("Sample rate: {}", sweep_device_get_sample_rate(device, &err));
+        check(err).unwrap();
+
         println!("start scanning");
         sweep_device_start_scanning(device, &err);
         check(err).unwrap();
 
+        let scan = sweep_device_get_scan(device, &err);
+        check(err).unwrap();
+
+        let sample_count = sweep_scan_get_number_of_samples(scan);
+
+        for n in 0..sample_count {
+          let angle = sweep_scan_get_angle(scan, n);
+          let distance = sweep_scan_get_distance(scan, n);
+          let signal = sweep_scan_get_signal_strength(scan, n);
+          println!("Angle {}, Distance {}, Signal Strength: {}", angle, distance, signal);
+        }
+
+        println!("stop scanning");
+        sweep_device_stop_scanning(device, &err);
+        check(err).unwrap();
 
         }
     }
